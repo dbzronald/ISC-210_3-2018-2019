c3c8653852b87e5ea85c7cc3e9a6299882e669f0
Ronald Mariotti <r.mariotti19@gmail.com>
2019-05-29 18:02:25 -0400

    Ball y Player scripts

---
+++ PirataRPG_3-2018-2019/Assets/Scripts/BallBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehaviour : MonoBehaviour
+{
+    private bool? isLeftPlayer;
+
+    private float _startingSpeed = 5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpadateBallState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isLeftPlayer != null && Input.GetButtonDown("Fire1"))
+        {
+            if (gameObject.transform.parent.transform.position.x < gameObject.transform.position.x)
+            {
+                _startingSpeed = -_startingSpeed;
+            }
+
+            gameObject.transform.SetParent(null);
+
+            gameObject.GetComponent<Rigidbody>().velocity = new Vector3(-_startingSpeed,_startingSpeed * (Random.Range(0,2) == 0 ? 1 : -1));
+
+            isLeftPlayer = null;
+        }
+        
+    }
+
+    public void UpadateBallState()
+    {
+        switch (transform.parent.name)
+        {
+            case "LeftPlayer":
+                isLeftPlayer = true;
+                break;
+            case "RightPlayer":
+                isLeftPlayer = false;
+                break;
+            default:
+                isLeftPlayer = null;
+                break;
+
+        }
+
+    }
+}
--- PirataRPG_3-2018-2019/Assets/Scripts/PongPlayerBehaviour.cs
+++ PirataRPG_3-2018-2019/Assets/Scripts/PongPlayerBehaviour.cs
@@ -11,22 +11,33 @@ public class PongPlayerBehaviour : MonoBehaviour
     private Vector3 _deltaPos;
 
     private const float UPPPERLIMIT = 3.5f, LOWERLIMIT = -3.5f;
+
+    private static bool _onePlayer;
+
+    private GameObject _ball;
     // Start is called before the first frame update
 
     private void Awake()
     {
         _isLeftPlayer = gameObject.name == "LeftPlayer";
+        _ball = GameObject.Find("Ball");
     }
     void Start()
     {
-        
+        _onePlayer = true; // Esto s para probar. Se supone que debe
+        // ser elegido por el jugador.
     }
 
     // Update is called once per frame
     void Update()
     {
-        _deltaPos = new Vector3(0, (_isLeftPlayer ? Input.GetAxis("LeftPlayer") : Input.GetAxis("Vertical")) * _speed * Time.deltaTime);
+        if (_onePlayer && ! _isLeftPlayer)
+        {
+        //    transform.position = new Vector3(transform.position.x, Mathf.Lerp(gameObject.transform.position.x, _ball.transform.position.x, 0.5f));
+        }
+        _deltaPos = new Vector3(0, (_isLeftPlayer ? Input.GetAxis("LeftPlayer") :  Input.GetAxis("Vertical")) * _speed * Time.deltaTime);
         transform.Translate(_deltaPos);
-        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, LOWERLIMIT, UPPPERLIMIT), transform.position.z);
+        transform.position = new Vector3(transform.position.x, 
+            Mathf.Clamp(transform.position.y, LOWERLIMIT, UPPPERLIMIT), transform.position.z);
     }
 }
